33db876bf44d260fa6a4600d0348b16a57314b7a
Karol Mazurek <kmazurek93@gmail.com>
2016-04-03 17:02:35 +0200

    initialize repository

---
+++ src/dszi/pkg2016/Dszi2016.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dszi.pkg2016;
+
+/**
+ *
+ * @author Karol Mazurek <kmazurek93@gmail.com>
+ */
+public class Dszi2016 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
